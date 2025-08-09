06748131e8b98ad87ef3db357bddb54781807f1e
Sn0wF1re <mokayaclinton34@gmail.com>
2025-08-09 12:50:25 +0300

    chore: remove unnecessary Card UI component for now

--- app/components/ui/Card.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="group cursor-pointer overflow-hidden border-[#c5aa5e]/20 hover:border-[#c5aa5e] transition-all rounded-md bg-background p-4 shadow-sm">
-    <slot />
-  </div>
-</template>
-
-<script setup>
-</script>
